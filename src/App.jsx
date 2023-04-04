80a97520e2154e9cff531548610ef286dd19272f
Jd1503 <jaideepdutta15@gmail.com>
2023-04-05 03:34:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
+import SoundSection from "./components/Soundsection";
+import DisplaySection from "./components/DisplaySection";
+import WebgiViewer from "./components/WebgiViewer";
+import Loader from "./components/Loader";
+import { useRef } from "react";
+
+function App() {
+  const WebgiViewerRef = useRef();
+  const contentRef = useRef();
+
+  const handlePreview = () => {
+    WebgiViewerRef.current.triggerPreview();
+  };
+
+  return (
+    <div className="App">
+      <Loader />
+      <div ref={contentRef} id="content">
+        <Nav />
+        <Jumbotron />
+        <SoundSection />
+        <DisplaySection triggerPreview={handlePreview} />
+      </div>
+
+      <WebgiViewer contentRef={contentRef} ref={WebgiViewerRef} />
+    </div>
+  );
+}
+
+export default App;
